6ad7999f8ae0ac62da0acbe0d6d46112bf181db8
Uday Chadha <Uday.Chadha@ey.com>
2019-01-24 17:24:36 -0800

    added basic boilerplate electron window. In process of defining html skeleton

---
+++ main.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow, Menu} = require('electron')
+const shell = require('electron').shell
+const ipc = requier('electron').ipc
+
+
+let win
+
+function createWindow() {
+    win - new BrowserWindow( {width: 1000, height: 700})
+
+    win.loadFile('src/index.html')
+
+    win.on('closed', () => {
+        win = null
+    })
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu:[
+                {
+                    label: 'Exit',
+                    click() {
+                        app.quit()
+                    }
+                }
+            ]
+        }
+    ])
+    Menu.setApplicationMenu(menu)
+}
+
+app.on('ready', createWindow)
+
